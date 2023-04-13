5e10437bc4fe291cdf07ef0270735f1041c34339
Ali <101870797+alitarakci2@users.noreply.github.com>
2023-04-14 02:00:10 +0300

    Implementing rendering list with map and filter

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -10,6 +10,7 @@ function ExpenseItem(props){
    
 
     return (
+        <li>
         <Card className="expense-item">
             <ExpenseDate date = {props.date}/>
             <div className="expense-item__description">
@@ -17,6 +18,7 @@ function ExpenseItem(props){
                 <div className="expense-item__price">${props.amount}</div>
             </div>
         </Card>
+        </li>
     );
 }
 
